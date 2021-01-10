86dcb38925ea63fadc5667148578ca04b84f84a6
Sienna Hu <xh244@cornell.edu>
2021-01-10 12:05:01 +0800

    Refactor Services to Common Interface (Get rid of redundant codes). Close #11.

--- pet-clinic-data/src/main/java/guru/springframework/sfgperclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgperclinic/services/PetService.java
@@ -2,10 +2,5 @@ package guru.springframework.sfgperclinic.services;
 
 import guru.springframework.sfgperclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long>{
 }
--- pet-clinic-data/src/main/java/guru/springframework/sfgperclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgperclinic/services/VetService.java
@@ -2,10 +2,6 @@ package guru.springframework.sfgperclinic.services;
 
 import guru.springframework.sfgperclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long>{
 
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
 }
