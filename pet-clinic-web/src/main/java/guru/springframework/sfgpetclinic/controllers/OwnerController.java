58b20e667bacc1572e75f0632586086f9316bcd1
Sienna Hu <xh244@cornell.edu>
2021-01-12 10:42:33 +0800

    apple SPC layout to my owner list page

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -15,7 +15,7 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
-    @RequestMapping({"", "/", "/index", "/index.html"})
+    @RequestMapping({"", "/", "/index", "/index.html", "/find"})
     public String listOwners(Model model){
 
         model.addAttribute("owners", ownerService.findAll());
