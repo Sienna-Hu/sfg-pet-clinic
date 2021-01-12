25c749a7529eb1ed57a893e7f232636d8fb97d7c
Sienna Hu <xh244@cornell.edu>
2021-01-12 18:59:05 +0800

    Convert Person, Owner, Pet, and PetType to JPA Entities. Close #36

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -1,12 +1,24 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity{
 
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+
+    @Column(name = "name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @Column(name = "birth_date")
     private LocalDate birthDate;
 
     public PetType getPetType() {
